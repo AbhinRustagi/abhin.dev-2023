1a5d14d85000b9f586b259f4b4937e70484fdb6a
AbhinRustagi <rustagi.abhin@gmail.com>
2023-04-03 02:18:50 +0530

    feat: switch to inter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import type { AppProps } from 'next/app'
+import '@fontsource/inter'
+import '@fontsource/inter/300.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 import { DefaultSeo, NextSeo } from 'next-seo'
+import type { AppProps } from 'next/app'
+import 'react-tooltip/dist/react-tooltip.css'
+import { ThemeProvider } from '~/context'
 import { defaultSeoProps } from '~/data'
 import { GlobalStyles } from '~/theme'
-import 'react-tooltip/dist/react-tooltip.css'
-import '@fontsource/inconsolata'
-import '@fontsource/inconsolata/500.css'
-import '@fontsource/inconsolata/600.css'
-import { ThemeProvider } from '~/context'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
