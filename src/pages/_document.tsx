8aa85c150b6cdec852a5f20c9aba6e0ff0e51df5
AbhinRustagi <rustagi.abhin@gmail.com>
2023-04-19 02:01:21 +0530

    feat: add emotion server side rendering

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,71 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
 import Script from 'next/script'
+import React, { ReactElement } from 'react'
+import createCache, { EmotionCache } from '@emotion/cache'
+import createEmotionServer from '@emotion/server/create-instance'
+import { AppType } from 'next/dist/shared/lib/utils'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-${`TODO: Replace GA ID Here`}`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
+interface DocumentProps extends DocumentInitialProps {
+  emotionStyleTags: React.ReactNode[]
+}
+
+function createEmotionCache() {
+  return createCache({ key: 'ar', prepend: true })
+}
+
+export default class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentProps> {
+    const originalRenderPage = ctx.renderPage
+
+    const cache = createEmotionCache()
+    const { extractCriticalToChunks } = createEmotionServer(cache)
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (
+          App: AppType | React.ComponentType<{ emotionCache: EmotionCache }>
+        ) =>
+          function EnhanceApp(props) {
+            return <App emotionCache={cache} {...props} />
+          },
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    const emotionStyles = extractCriticalToChunks(initialProps.html)
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        key={style.key}
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ))
+
+    return {
+      ...initialProps,
+      emotionStyleTags,
+    }
+  }
+
+  render(): ReactElement {
+    return (
+      <Html lang="en">
+        <Head>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-${`TODO: Replace GA ID Here`}`}
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
@@ -21,13 +73,14 @@ export default function Document() {
                     page_path: window.location.pathname,
                   });
               `,
-          }}
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
