20d9c7627c738694650ffa72388664debebc875a
Abhin Rustagi <55152006+AbhinRustagi@users.noreply.github.com>
2023-04-24 15:58:55 +0530

    feat: make footer abstract

--- src/components/Layout/components/Footer/index.tsx
+++ src/components/Layout/components/Footer/index.tsx
@@ -2,6 +2,44 @@ import { Wrapper, Row, Col } from './styled'
 import { footerInformationMenu, footerSocials } from '~/data'
 import Link from 'next/link'
 import { makeKey } from '~/utils'
+import { ReactFC } from '~/types'
+
+interface ColumnProps {
+  title: string
+  items: any[]
+  titleKey: string
+  linkKey: string
+}
+
+const data = [
+  {
+    title: 'Information',
+    items: footerInformationMenu,
+    titleKey: 'title',
+    linkKey: 'path',
+  },
+  {
+    title: 'Socials',
+    items: footerSocials,
+    titleKey: 'name',
+    linkKey: 'link',
+  },
+]
+
+const Column: ReactFC<ColumnProps> = (props) => {
+  const { title, titleKey, linkKey, items } = props
+
+  return (
+    <Col.wrapper>
+      <Col.title>{title}</Col.title>
+      {items.map((item) => (
+        <Col.item key={makeKey('footer', title, item[titleKey])}>
+          <Link href={item[linkKey]}>{item[titleKey]}</Link>
+        </Col.item>
+      ))}
+    </Col.wrapper>
+  )
+}
 
 export const Footer = () => {
   return (
@@ -12,22 +50,9 @@ export const Footer = () => {
         drop me a message! I&apos;m always excited to connect with new people.
       </p>
       <Row>
-        <Col.wrapper>
-          <Col.title>Information</Col.title>
-          {footerInformationMenu.map((menuItem) => (
-            <Col.item key={makeKey('footer-info', menuItem.title)}>
-              <Link href={menuItem.path}>{menuItem.title}</Link>
-            </Col.item>
-          ))}
-        </Col.wrapper>
-        <Col.wrapper>
-          <Col.title>Social</Col.title>
-          {footerSocials.map((social) => (
-            <Col.item key={makeKey('footer-social', social.name)}>
-              <Link href={social.link}>{social.name}</Link>
-            </Col.item>
-          ))}
-        </Col.wrapper>
+        {data.map((col) => (
+          <Column key={makeKey('footer-col', col.title)} {...col} />
+        ))}
       </Row>
       <small>&copy; Abhin Rustagi | 2023</small>
     </Wrapper>
