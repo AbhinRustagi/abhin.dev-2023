a0d22f9666186991f7aba12ce5a7422ab8577579
AbhinRustagi <rustagi.abhin@gmail.com>
2023-06-09 16:52:55 +0530

    feat: add seo builders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,30 @@ import { ThemeProvider } from '~/context/theme'
 import { defaultSeoProps } from '~/data'
 import GlobalLayout from '~/layout/global'
 import { GlobalStyles } from '~/theme'
+import { buildPageSeo } from '~/utils/seo'
+import { useRouter } from 'next/router'
 
 interface CustomAppProps {
   data: any
-  seo: NextSeoProps
+  seo?: NextSeoProps
 }
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<CustomAppProps>) {
+  const router = useRouter()
+
+  const pageSeo = buildPageSeo({
+    title: pageProps.seo?.title || '',
+    description: pageProps.seo?.description || '',
+    path: router.pathname,
+  })
+
   return (
     <>
       <DefaultSeo {...defaultSeoProps} />
-      <NextSeo {...pageProps.seo} />
+      <NextSeo {...pageSeo} />
       <ThemeProvider>
         <MenuStateContextProvider>
           <GlobalStyles />
